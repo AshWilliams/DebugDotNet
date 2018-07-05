d447cad7581e8b991122d6ec2c2f55a68a5d466e
Robert Rozas Navarro <robert.rozas.n@gmail.com>
2018-07-05 10:50:11 -0400

    Se añade fuentes de ejemplo e installers de DebugDiag tools

---
+++ TestAppFileNotFound/TestAppFileNotFound/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestAppFileNotFound
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //This file doesn't exist, ergo, should throw an exception
+                string text = System.IO.File.ReadAllText(@"C:\Users\Public\TestFolder\WriteText.txt");
+
+                // Display the file contents to the console. Variable text is a string.
+                System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
